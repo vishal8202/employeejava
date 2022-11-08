515b627ad92de5fd80270cff1ae0cd4d4289bdd5
vishal8202 <vishalvijayan222@gmail.com>
2022-11-09 02:16:40 +0530

    commit

--- src/employee.java
+++ src/employee.java
@@ -18,6 +18,48 @@ public class Employee {
             System.out.println("6. Exit ");
             System.out.println("Enter your choice: ");
             choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Add Employee");
+                    System.out.println("Enter the Employee code: ");
+                    empcode = input.nextInt();
+                    System.out.println("Enter Employee name: ");
+                    empname = input.next();
+                    System.out.println("Enter the designation: ");
+                    empdes = input.next();
+                    System.out.println("Enter the salary: ");
+                    empsalary = input.nextInt();
+                    System.out.println("Enter the Company name: ");
+                    compname = input.next();
+                    System.out.println("Enter the Phone Number: ");
+                    empphone = input.next();
+                    System.out.println("Enter the Email Id: ");
+                    empemail = input.next();
+                    System.out.println("Enter the password: ");
+                    emppassword = input.next();
+
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root", "");
+                        String sql = "INSERT INTO `employee`(`emp_code`, `emp_name`, `designation`, `salary`, `companyname`, `phone`, `emailid`, `password`) VALUES (?,?,?,?,?,?,?,?)";
+                        PreparedStatement stmt = con.prepareStatement(sql);
+                        stmt.setInt(1, empcode);
+                        stmt.setString(2, empname);
+                        stmt.setString(3, empdes);
+                        stmt.setInt(4, empsalary);
+                        stmt.setString(5, compname);
+                        stmt.setString(6, empphone);
+                        stmt.setString(7, empemail);
+                        stmt.setString(8, emppassword);
+                        stmt.executeUpdate();
+
+
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    break;
+            }
         }
     }
 }
