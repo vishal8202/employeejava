3945056fe0bca7e6ee7816cae4833b8edcbe37a5
vishal8202 <vishalvijayan222@gmail.com>
2022-11-09 10:50:22 +0530

    update

--- src/employee.java
+++ src/employee.java
@@ -91,34 +91,33 @@ public class Employee {
                         System.out.println(e);
                     }
                     break;
-                case 3:
-                    System.out.println("Search for an Employee");
+                case 4:
+                    System.out.println("Update a Employee");
                     System.out.println("Enter the Employee code: ");
                     empcode = input.nextInt();
-                    try{
+                    System.out.println("Enter Employee name to update: ");
+                    empname = input.next();
+                    System.out.println("Enter the designation to update: ");
+                    empdes = input.next();
+                    System.out.println("Enter the salary to update: ");
+                    empsalary = input.nextInt();
+                    System.out.println("Enter the Company name to update: ");
+                    compname = input.next();
+                    System.out.println("Enter the Phone Number to update: ");
+                    empphone = input.next();
+                    System.out.println("Enter the Email Id to update: ");
+                    empemail = input.next();
+                    System.out.println("Enter the password to update: ");
+                    emppassword = input.next();
+
+                    try {
                         Class.forName("com.mysql.jdbc.Driver");
                         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root", "");
-                        String sql = "SELECT `emp_name`, `designation`, `salary`, `companyname`, `phone`, `emailid`, `password` FROM `employee` WHERE `emp_code`="+String.valueOf(empcode);
+                        String sql = "UPDATE `employee` SET `emp_name`='"+empname+"',`designation`='"+empdes+"',`salary`='"+empsalary+"',`companyname`='"+compname+"',`phone`='"+empphone+"',`emailid`='"+empemail+"',`password`='"+emppassword+"' WHERE `emp_code` = "+String.valueOf(empcode);
 
                         Statement stmt = con.createStatement();
-                        ResultSet rs = stmt.executeQuery(sql);
-                        while(rs.next()){
-                            String fetchEmpName = rs.getString("empname");
-                            String fetchEmpDes = rs.getString("designation");
-                            String fetchEmpSalary = rs.getString("salary");
-                            String fetchCompName = rs.getString("companyname");
-                            String fetchPhone = rs.getString("phone");
-                            String fetchEmail = rs.getString("emailid");
-
-                            System.out.println("Employee Name: "+fetchEmpName);
-                            System.out.println("Employee Designation: "+fetchEmpDes);
-                            System.out.println("Employee Salary: "+fetchEmpSalary);
-                            System.out.println("Employee CompanyName: "+fetchCompName);
-                            System.out.println("Employee Phone Number: "+fetchPhone);
-                            System.out.println("Employee Email Id: "+fetchEmail);
-
-                        }
-
+                        stmt.executeUpdate(sql);
+                        System.out.println("Data updated successfully");
                     }
                     catch (Exception e){
                         System.out.println(e);
