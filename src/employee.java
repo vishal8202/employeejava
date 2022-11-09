659b13b6fb6cf101fef50eed9016c8fb86356f1b
vishal8202 <vishalvijayan222@gmail.com>
2022-11-09 10:02:23 +0530

    view

--- src/employee.java
+++ src/employee.java
@@ -59,6 +59,38 @@ public class Employee {
                         System.out.println(e);
                     }
                     break;
+                case 2:
+                    System.out.println("Viewl All Employees");
+                    try{
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb","root","");
+                        String sql ="SELECT `emp_code`, `emp_name`, `designation`, `salary`, `companyname`, `phone`, `emailid`, `password` FROM `employee` ";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(sql);
+                        while(rs.next()){
+                            String fetchEmpCode = rs.getString("empcode");
+                            String fetchEmpName = rs.getString("empname");
+                            String fetchEmpDes = rs.getString("designation");
+                            String fetchEmpSalary = rs.getString("salary");
+                            String fetchCompName = rs.getString("companyname");
+                            String fetchPhone = rs.getString("phone");
+                            String fetchEmail = rs.getString("emailid");
+                            String fetchPassword = rs.getString("password");
+                            System.out.println("Employee Code: "+fetchEmpCode);
+                            System.out.println("Employee Name: "+fetchEmpName);
+                            System.out.println("Employee Designation: "+fetchEmpDes);
+                            System.out.println("Employee Salary: "+fetchEmpSalary);
+                            System.out.println("Employee CompanyName: "+fetchCompName);
+                            System.out.println("Employee Phone Number: "+fetchPhone);
+                            System.out.println("Employee Email Id: "+fetchEmail);
+                            System.out.println("Employee Password: "+fetchPassword+"\n");
+
+                        }
+                    }
+                    catch (Exception e){
+                        System.out.println(e);
+                    }
+                    break;
             }
         }
     }
